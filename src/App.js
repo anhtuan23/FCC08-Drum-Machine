09f8d1f70bdb278ab6e4165311f1e7e088a4a15a
Do Tuan Anh <dotuananh.dta@gmail.com>
2019-01-06 19:14:10 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="app">
+        <DrumMachine />
+      </div>
+    );
+  }
+}
+
+class DrumMachine extends React.Component {
+  render() {
+    return (
+      <div id="drum-machine">
+        <DrumContainer />
+        <Controller/>
+      </div>
+    );
+  }
+}
+
+class DrumContainer extends React.Component {
+  render() {
+    return (
+      <div id="drum-container">
+        <DrumPad text={'Q'}/>
+        <DrumPad text={'W'}/>
+        <DrumPad text={'E'}/>
+        <DrumPad text={'A'}/>
+        <DrumPad text={'S'}/>
+        <DrumPad text={'D'}/>
+        <DrumPad text={'Z'}/>
+        <DrumPad text={'X'}/>
+        <DrumPad text={'C'}/>
+      </div>
+    );
+  }
+}
+
+class DrumPad extends React.Component{
+  render(){
+    return(
+      <div className="drum-pad">{this.props.text}</div>
+    )
+  }
+}
+
+class Controller extends React.Component {
+  render(){
+    return(
+      <div id="controller">
+
       </div>
     );
   }
