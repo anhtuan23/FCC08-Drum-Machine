7b10caf7dd5e99f14a2e1d1681821c5d8bfb03f9
Do Tuan Anh <dotuananh.dta@gmail.com>
2019-01-07 13:07:35 +0700

    Updates

---
+++ precache-manifest.71963a2e23c1ba5f71e75bd5d6ad3a21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cea3b9361c25ab30d06",
+    "url": "/FCC08-Drum-Machine/static/js/runtime~main.9cea3b93.js"
+  },
+  {
+    "revision": "a4c20c4268aeb18ac7da",
+    "url": "/FCC08-Drum-Machine/static/js/main.a4c20c42.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/FCC08-Drum-Machine/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "a4c20c4268aeb18ac7da",
+    "url": "/FCC08-Drum-Machine/static/css/main.51bffddf.chunk.css"
+  },
+  {
+    "revision": "1df9bdc47b0120f107fc64829e5d7077",
+    "url": "/FCC08-Drum-Machine/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/FCC08-Drum-Machine/precache-manifest.71963a2e23c1ba5f71e75bd5d6ad3a21.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/FCC08-Drum-Machine/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
