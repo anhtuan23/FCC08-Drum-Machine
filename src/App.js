cf883761739bdf975d5c814200f77eba50e5e776
Do Tuan Anh <dotuananh.dta@gmail.com>
2019-01-07 11:23:41 +0700

    make it fcc text compliant

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { sfx } from './sound-info.js';
 
 class App extends React.Component {
   render() {
@@ -12,47 +13,103 @@ class App extends React.Component {
 }
 
 class DrumMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      key: '',
+      name: ''
+    }
+    this.handlePadClick = this.handlePadClick.bind(this);
+  }
+
+  handlePadClick(key) {
+    this.setState(
+      {
+        key,
+        name: sfx[key].name
+      }
+    );
+  }
+
   render() {
     return (
       <div id="drum-machine">
-        <DrumContainer />
-        <Controller/>
+        <DrumContainer handlePadClick={this.handlePadClick} sfx={sfx} />
+        <Controller soundName={this.state.name} />
       </div>
     );
   }
 }
 
 class DrumContainer extends React.Component {
+
   render() {
     return (
       <div id="drum-container">
-        <DrumPad text={'Q'}/>
-        <DrumPad text={'W'}/>
-        <DrumPad text={'E'}/>
-        <DrumPad text={'A'}/>
-        <DrumPad text={'S'}/>
-        <DrumPad text={'D'}/>
-        <DrumPad text={'Z'}/>
-        <DrumPad text={'X'}/>
-        <DrumPad text={'C'}/>
+        <DrumPad keyName={'Q'} handlePadClick={this.props.handlePadClick} src={this.props.sfx.Q.src} />
+        <DrumPad keyName={'W'} handlePadClick={this.props.handlePadClick} src={this.props.sfx.W.src} />
+        <DrumPad keyName={'E'} handlePadClick={this.props.handlePadClick} src={this.props.sfx.E.src} />
+        <DrumPad keyName={'A'} handlePadClick={this.props.handlePadClick} src={this.props.sfx.A.src} />
+        <DrumPad keyName={'S'} handlePadClick={this.props.handlePadClick} src={this.props.sfx.S.src} />
+        <DrumPad keyName={'D'} handlePadClick={this.props.handlePadClick} src={this.props.sfx.D.src} />
+        <DrumPad keyName={'Z'} handlePadClick={this.props.handlePadClick} src={this.props.sfx.Z.src} />
+        <DrumPad keyName={'X'} handlePadClick={this.props.handlePadClick} src={this.props.sfx.X.src} />
+        <DrumPad keyName={'C'} handlePadClick={this.props.handlePadClick} src={this.props.sfx.C.src} />
       </div>
     );
   }
 }
 
-class DrumPad extends React.Component{
-  render(){
-    return(
-      <div className="drum-pad">{this.props.text}</div>
+class DrumPad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.playSound = this.playSound.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  handleKeyPress(e) {
+    if (e.keyCode === this.props.keyName.charCodeAt(0)) {
+      const element = document.getElementById(`drum-pad-${this.props.keyName}`);
+      element.click();
+    }
+  }
+
+  playSound() {
+    const audio = document.getElementById(this.props.keyName);
+    audio.click();
+    audio.currentTime = 0;
+    audio.play();
+
+    //make click effect
+    const element = audio.parentNode;
+    element.style.backgroundColor = "#6e788d";
+    setTimeout(() => {
+      element.style.backgroundColor = "#474e5c";
+    }, 100);
+
+    this.props.handlePadClick(this.props.keyName);
+  }
+
+  render() {
+    return (
+      <div className="drum-pad" id={`drum-pad-${this.props.keyName}`} onClick={this.playSound}>
+        {this.props.keyName}
+        <audio src={this.props.src} className="clip" id={this.props.keyName} />
+      </div>
+
     )
   }
 }
 
 class Controller extends React.Component {
-  render(){
-    return(
-      <div id="controller">
-
+  render() {
+    return (
+      <div id="display">
+        {this.props.soundName}
       </div>
     );
   }
---
+++ src/sound-info.js
@@ -0,0 +1,38 @@
+export const sfx = {
+    Q: {
+        name: 'clap',
+        src: 'https://raw.githubusercontent.com/anhtuan23/FCC08-Drum-Machine/master/public/sound-effect/clap-808.wav'
+    },
+    W: {
+        name: 'cowbell',
+        src: 'https://raw.githubusercontent.com/anhtuan23/FCC08-Drum-Machine/master/public/sound-effect/cowbell-808.wav'
+    },
+    E: {
+        name: 'crash',
+        src: 'https://raw.githubusercontent.com/anhtuan23/FCC08-Drum-Machine/master/public/sound-effect/crash-808.wav'
+    },
+    A: {
+        name: 'hihat',
+        src: 'https://raw.githubusercontent.com/anhtuan23/FCC08-Drum-Machine/master/public/sound-effect/hihat-808.wav'
+    },
+    S: {
+        name: 'kick',
+        src: 'https://raw.githubusercontent.com/anhtuan23/FCC08-Drum-Machine/master/public/sound-effect/kick-808.wav'
+    },
+    D: {
+        name: 'openhat',
+        src: 'https://raw.githubusercontent.com/anhtuan23/FCC08-Drum-Machine/master/public/sound-effect/openhat-808.wav'
+    },
+    Z: {
+        name: 'perc',
+        src: 'https://raw.githubusercontent.com/anhtuan23/FCC08-Drum-Machine/master/public/sound-effect/perc-808.wav'
+    },
+    X: {
+        name: 'ride',
+        src: 'https://raw.githubusercontent.com/anhtuan23/FCC08-Drum-Machine/master/public/sound-effect/ride-acoustic01.wav'
+    },
+    C: {
+        name: 'shaker',
+        src: 'https://raw.githubusercontent.com/anhtuan23/FCC08-Drum-Machine/master/public/sound-effect/shaker-analog.wav'
+    },
+};
